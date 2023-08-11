9d628e0fe55be8f570fd522c5282981cc67f2743
oliverchuks <oliverchuks87@gmail.com>
2023-08-10 17:19:17 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(z));
 	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(q));
 	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(w));
-	return(0);
+	return (0);
 }
